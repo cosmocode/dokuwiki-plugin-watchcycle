68716d5d0e39962f606ac50450ee573ae1b63d87
Szymon Olewniczak <solewniczak@rid.pl>
2018-03-30 11:43:03 +0200

    pluginwiz

---
+++ lang/en/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * English language file for watchcycle plugin
+ *
+ * @author Szymon Olewniczak <dokuwiki@cosmocode.de>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
+
+
+
+//Setup VIM: ex: et ts=4 :
