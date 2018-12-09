851c0fb01534d412d5387d7ace1a936d850fc79d
Christoph Ziehr <christoph@christoph-ziehr.de>
2018-12-09 21:29:13 +0100

    German localization added

---
+++ lang/de/lang.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * German language file for watchcycle plugin
+ *
+ * @author Christoph Ziehr <info@einsatzleiterwiki.de>
+ */
+// Menüeintrag für Admin-Plugins
+$lang['menu'] = 'Wiederkehrende Überprüfungen verwalten';
+// custom language strings for the plugin
+$lang['maintained by'] = 'Wird überprüft von %s.';
+$lang['last check'] = 'Letzte Überprüfung vor %d Tagen.';
+$lang['check needed'] = 'Überprüfung benötigt!';
+$lang['since last check'] = '%s seit letzter Überprüfung.';
+$lang['never checked'] = 'Noch nicht überprüft.';
+$lang['change singular'] = 'Änderung';
+$lang['change plural'] = 'Änderungen';
+$lang['cb only maintained pages'] = 'Nur überprüfte Seiten';
+// Admin
+$lang['search page'] = 'Seite suchen';
+$lang['btn filter'] = 'Filter';
+$lang['show outdated only'] = 'nur veraltete zeigen';
+$lang['h page'] = 'Seite';
+$lang['h maintainer'] = 'Prüfer';
+$lang['h cycle'] = 'Intervall in Tagen';
+$lang['h current'] = 'Tage seit letzter Überprüfung';
+$lang['h uptodate'] = 'aktuell?';
+// Mail
+$lang['mail subject'] = 'Seitenüberprüfung erforderlich';
+$lang['mail body'] = '%s hat das Prüfungs-Intervall überschritten und benötigt eine erneute Überprüfung.';
+// Fehler
+$lang['error mail'] = 'Kann keine Email an den Prüfer senden.';
+$lang['error sqlite missing'] = 'Das watchcycle-Plugin benötigt das <a href="https://www.dokuwiki.org/plugin:sqlite">sqlite plugin</a> um zu funktionieren.';
+$lang['title toolbar button'] = 'Syntax für wiederkehrende Überprüfung einfügen';
+$lang['js']['label_username'] = 'Benutzername';
+$lang['js']['label_cycle_length'] = 'Intervall für Überprüfung';
+$lang['js']['button_insert'] = 'Einfügen';
+$lang['js']['button_cancel'] = 'Abbrechen';
